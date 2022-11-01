11eee0bf345e4c7dd88ce1ac0d142df3d729a953
HamzaMansouri7 <mansouris2020@gmail.com>
2022-11-02 00:03:58 +0100

    changement

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-
+app.use(express.static(__dirname + '/dist/hamzamansouri'));
 app.get('/*', function(req,res) {
-    
-res.sendFile(__dirname+'/dist/index.html');
-});
-
-// Start the app by listening on the default Heroku port
+res.sendFile(path.join(__dirname+
+'/dist/hamzamansouri/index.html'));});
 app.listen(process.env.PORT || 8080);
